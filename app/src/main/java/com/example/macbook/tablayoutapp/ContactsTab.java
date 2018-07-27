c06a4faaf46af7474b64b11a68a74229af0487ed
MAcBook <MAcBook@macbook.lan>
2018-07-27 19:18:27 +0400

    Initial commit

---
+++ app/src/main/java/com/example/macbook/tablayoutapp/ContactsTab.java
@@ -0,0 +1,20 @@
+package com.example.macbook.tablayoutapp;
+
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class ContactsTab extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.contacts, container, false);
+        return rootView;
+    }
+
+}
